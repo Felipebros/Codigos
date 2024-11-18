409d3b187adc3e16ad398d89b44a20aea42dfc23
felipe.silveira <felipe.silveira@extreme.digital>
2024-11-18 11:51:14 -0300

    feat: adicionar comando de listar programas instalados no linux

--- comandos_importantes.sh
+++ comandos_importantes.sh
@@ -1,5 +1,5 @@
 tree -if diretório/  # lista os arquivos dessa maneira diretório/pasta/arquivo-teste.txt
-history -d 23-33  # deleta o histórico de comando do 4 ao 6 depois disso precisa executar history -w para salvar
+history -d 24-34  # deleta o histórico de comando do 4 ao 6 depois disso precisa executar history -w para salvar
 history -w  # salva o histórico de comandos alterados
 sed -i '12,22d' ~/.bash_history
 git log --pretty=format:"- %s" --reverse  # retorna uma lista de commits do mais antigo para o mais novo
@@ -19,4 +19,5 @@ sed -i -e '2,5d'  # deleta a linha 2 até a linha 5 e já salva o arquivo
 fc -l 1 3  # mostra os históricos de comandos do comando 1 ao 3
 . .venv/bin/activate
 . venv/bin/activate
+dpkg -l  # lista os programas instalados no linux
 echo 'Fim dos comandos importantes'
